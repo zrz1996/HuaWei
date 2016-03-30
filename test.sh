d7fb9d8c9e2fe58a3d19dc78344f910570cdd356
zrz1996 <zrz96@163.com>
2016-03-30 23:10:06 +0800

    modify test.sh
    auto compile /checker/check.cpp

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+if [ $(uname) == "Darwin" ]; then
+	g++-4.9 -std=c++11 ./checker/check.cpp -o ./checker/check -O2
+else
+	g++ -std=c++11 ./checker/check.cpp -o ./checker/check -O2
+fi
 python ./checker/test.py
